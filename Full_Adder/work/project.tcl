3bb96e0b25462d77967b881214a609cc27e610ff
ongkahyuan <52844020+ongkahyuan@users.noreply.github.com>
2020-10-08 11:58:59 +0800

    Base project

---
+++ Full_Adder/work/project.tcl
@@ -0,0 +1,17 @@
+set projDir "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/vivado"
+set projName "IntroToFPGAPart1"
+set topName top
+set device xc7a35tftg256-1
+if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
+create_project $projName "$projDir/$projName" -part $device
+set_property design_mode RTL [get_filesets sources_1]
+set verilogSources [list "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/au_top_0.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/full_adder_1.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/checker_2.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/reset_conditioner_3.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/edge_detector_4.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/button_conditioner_5.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/multi_seven_seg_6.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/singlecounter_7.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/pipeline_8.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/counter_9.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/seven_seg_10.v" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/verilog/decoder_11.v" ]
+import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
+set xdcSources [list "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/work/constraint/custom.xdc" "C:/Users/Admin/Documents/alchitry/IntroToFPGAPart1/constraint/file.xdc" ]
+read_xdc $xdcSources
+set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
+update_compile_order -fileset sources_1
+launch_runs -runs synth_1 -jobs 8
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
